8ddcff4c3483f694858a7ddefcde9503d1a02e49
Peter Gundel <mail@petergundel.de>
2018-03-20 00:09:16 +0100

    WIP Add types for mjml

---
+++ types/mjml/index.d.ts
@@ -0,0 +1,22 @@
+// Type definitions for mjml 4.0
+// Project: https://github.com/mjmlio/mjml
+// Definitions by: store2be <https://github.com/store2be>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
+
+/// <reference types="node" />
+
+export namespace JSX {
+  interface IntrinsicElements {
+    "mj-body": any;
+    "mj-button": any;
+    "mj-column": any;
+    "mj-container": any;
+    "mj-divider": any;
+    "mj-image": any;
+    "mj-section": any;
+    "mj-text": any;
+    "mj-wrapper": any;
+    mjml: any;
+  }
+}
---
+++ types/mjml/mjml-tests.ts
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+class Test extends React.Component {
+  render() {
+    return (
+      <mjml>
+        <mj-body>
+          <mj-section>
+            <mj-column>
+              <mj-text>Hello World!</mj-text>
+            </mj-column>
+          </mj-section>
+        </mj-body>
+      </mjml>
+    );
+  }
+}
